8acb1fdd6cef52573f44773287c8ee7d32f2996b
Eduardomak <eduardomak92@gmail.com>
2023-01-13 19:11:24 -0300

    feat: add counter test

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,12 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
-def test_counter():
-    pass
+def test_counter(path = "data/jobs.csv"):
+    assert count_ocurrences(path, "python") == 1639
+    assert count_ocurrences(path, "PYthOn") == 1639
+    assert count_ocurrences(path, "javascript") == 122
+    assert count_ocurrences(path, "marketing") == 1259
+    assert count_ocurrences(path, "engineering") == 2666
+    assert count_ocurrences(path, "node") == 38
+    assert count_ocurrences(path, "full-time") == 726
+    assert count_ocurrences(path, "part-time") == 175
