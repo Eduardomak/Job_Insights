32a8a41ef83e0da9a40ebdf7629387ee85366a0f
Eduardomak <eduardomak92@gmail.com>
2023-01-13 19:09:43 -0300

    feat: add salaries insight

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,7 +1,12 @@
 from typing import Union, List, Dict
-
+from src.insights.jobs import read
 
 def get_max_salary(path: str) -> int:
+    file = read(path)
+    return max([int(job["max_salary"])
+                for job in file
+                if len(job["max_salary"]) != 0
+                and job["max_salary"].isnumeric()])
     """Get the maximum salary of all jobs
 
     Must call `read`
@@ -20,6 +25,11 @@ def get_max_salary(path: str) -> int:
 
 
 def get_min_salary(path: str) -> int:
+    file = read(path)
+    return min([int(job["min_salary"])
+                for job in file
+                if len(job["min_salary"]) != 0
+                and job["min_salary"].isnumeric()])
     """Get the minimum salary of all jobs
 
     Must call `read`
@@ -38,6 +48,19 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
+    
+    if (
+        "min_salary" not in job
+        or "max_salary" not in job
+        or str(job["min_salary"]).isnumeric() == False
+        or str(job["max_salary"]).isnumeric() == False
+
+        or int(job["min_salary"]) > int(job["max_salary"])
+        or str(salary).lstrip("-").isnumeric() == False
+    ):
+        raise ValueError("Ops.. There is an invalid number or something is missing")
+
+    return int(job["min_salary"]) <= int(salary) <= int(job["max_salary"])
     """Checks if a given salary is in the salary range of a given job
 
     Parameters
@@ -60,13 +83,21 @@ def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
         If `job["min_salary"]` is greather than `job["max_salary"]`
         If `salary` isn't a valid integer
     """
-    raise NotImplementedError
+    """ raise NotImplementedError """
 
 
 def filter_by_salary_range(
     jobs: List[dict],
     salary: Union[str, int]
 ) -> List[Dict]:
+    all_jobs = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                all_jobs.append(job)
+        except ValueError:
+            pass
+    return all_jobs
     """Filters a list of jobs by salary range
 
     Parameters
